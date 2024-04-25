cbb7cd8ea537444b5f60b89e54cc02677cfd997b
Aerys12 <aerys.nsubuga@mail.utoronto.ca>
2024-04-25 17:03:33 -0400

    added custom user model and accounts app

--- photo_be/accounts/admin.py
+++ photo_be/accounts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from .models import CustomUser
 
-# Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = UserCreationForm
+    form = UserChangeForm
+    list_display = ['username', 'email', 'bio', 'website_url', 'is_staff']
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('bio', 'profile_picture', 'website_url')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('bio', 'profile_picture', 'website_url')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
