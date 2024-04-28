f18e5faead6e943c05aba5ada7126f3b47d4ffe8
Aerys12 <aerys.nsubuga@mail.utoronto.ca>
2024-04-27 22:39:51 -0400

    completed register api view

--- photo_be/accounts/serializers/register_serializer.py
+++ photo_be/accounts/serializers/register_serializer.py
@@ -5,17 +5,28 @@ User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password', 'first_name', 'last_name')
+        fields = ('username', 'email', 'password', 'confirm_password', 'first_name', 'last_name', 'bio', 'profile_picture', 'website_url')
+
+    def validate(self, data):
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError({"confirm_password": "Password fields didn't match."})
+        return data
 
     def create(self, validated_data):
+        validated_data.pop('confirm_password', None)  
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             first_name=validated_data.get('first_name', ''),
             last_name=validated_data.get('last_name', ''),
-            password=validated_data['password']
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None),
+            website_url=validated_data.get('website_url', None)
         )
         return user
+
