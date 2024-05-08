cfe4e7bc72a2902a2cf63016546127ec658cd718
Aerys12 <aerys.nsubuga@mail.utoronto.ca>
2024-05-08 08:16:35 -0400

    service categories added

--- photo_be/service/urls.py
+++ photo_be/service/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from service.views import view_services, view_service_categories, view_service_details
+from service.views import view_services, view_service_categories, view_service_details, view_all_service_categories
 
 app_name = 'service'
 urlpatterns = [
     path('list/', view_services.ViewServices.as_view(), name='view_services'),
     path('<int:parent_id>/list/', view_service_categories.ViewServiceCategories.as_view(), name='service_categories'),
     path('<int:id>/details/', view_service_details.ServiceDetails.as_view(), name='service_details'),
+    path('categories/list/', view_all_service_categories.ViewAllServiceCategories.as_view(), name='all_service_categories'),
     
 ]
